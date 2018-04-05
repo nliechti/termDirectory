07bbf8fe7f7a5d7b4497218a5af64fb93bd68788
Niklas Liechti <niklas.liechti@ergon.ch>
2018-04-05 17:26:40 +0200

    Disable debug errors for sure

--- config/prod.exs
+++ config/prod.exs
@@ -15,6 +15,7 @@ use Mix.Config
 # which you typically run after static files are built.
 config :termDirectory, TermDirectoryWeb.Endpoint,
   load_from_system_env: true,
+  debug_errors: false,
   url: [host: "example.com", port: 80],
   cache_static_manifest: "priv/static/cache_manifest.json"
 
