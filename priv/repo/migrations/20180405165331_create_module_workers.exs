828b9f7a1ba16781b48e145e4c17dd3b4f1f432e
Niklas Liechti <niklas.liechti@ergon.ch>
2018-04-06 10:42:58 +0200

    Migration for module_worker table

---
+++ priv/repo/migrations/20180405165331_create_module_workers.exs
@@ -0,0 +1,15 @@
+defmodule TermDirectory.Repo.Migrations.CreateModuleWorkers do
+  use Ecto.Migration
+
+  def change do
+    create table(:module_workers) do
+      add :teacher_id, references(:teachers, on_delete: :nothing)
+      add :module_id, references(:modules, on_delete: :nothing)
+
+      timestamps()
+    end
+
+    create index(:module_workers, [:teacher_id])
+    create index(:module_workers, [:module_id])
+  end
+end
